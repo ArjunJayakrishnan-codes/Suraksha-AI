3652dde7ed2dac1fc3ab87115e19c8b671ddb6c4
ArjunJayakrishnan-codes <arjk276@gmail.com>
2026-04-16 22:40:07 +0530

    chore: remove remaining Lovable traces from repository files

--- editor.ps1
+++
@@ -1,4 +0,0 @@
-param([string]$msgPath)
-$m = Get-Content -Raw -Encoding UTF8 $msgPath
-$m = $m -replace '(?i)lovable','Secure Gig Guardian'
-Set-Content -Encoding UTF8 -Value $m -Path $msgPath
--- rewrite-msg.ps1
+++
@@ -1,3 +0,0 @@
-param()
-$m = [Console]::In.ReadToEnd()
-$m -replace '(?i)lovable','Secure Gig Guardian' | Write-Output
